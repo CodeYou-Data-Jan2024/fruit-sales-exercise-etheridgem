ea712d9914b303115a5330a8e8a64a92d0c26fc4
Morgan Etheridge <kemmelingm@gmail.com>
2024-05-06 14:00:03 -0400

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-fruit_sales = pd.DataFrame(
-    {'Apples': [35, 41], 'Bananas': [21, 34]},
-      index = ['2017 sales', '2018 sales'])
+fruit_sales = pd.DataFrame({"Apples": ["35","41"], "Bananas": ["21","34"]}, index=["2017 Sales", "2018 Sales"])
+
 
 fruit_sales.to_csv('fruit.csv')
